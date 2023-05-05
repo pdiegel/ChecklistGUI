c2c03c61d6c76ee93affc8931bd7cb005f8fc40d
pdiegel <106894904+pdiegel@users.noreply.github.com>
2023-05-05 17:27:40 -0400

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Checklist
- 
+# Checklist Application
+
+This is a simple checklist application written in Python using the dearpygui library. It allows users to create a checklist of items and track their progress by checking and unchecking items. It also includes a timer feature that can be used to time events in the GUI.
+
+## Getting Started
+
+To run the application, you'll need to have Python 3 and the dearpygui library installed on your computer. You can install the library using pip:
+
+```
+pip install dearpygui
+```
+
+Once you have the library installed, you can run the application by executing the `main.py` file:
+
+```
+python main.py
+```
+
+## Usage
+
+When you run the application, a GUI window will appear with a list of checkboxes for each item in the checklist. You can check and uncheck items as you complete them, and the application will display the percentage of items completed.
+
+You can also use the timer feature to time events in the GUI. Click the "Start" button to start the timer, the "Pause" button to pause the timer, and the "Stop" button to stop and reset the timer. The elapsed time will be displayed in seconds, minutes, or hours depending on the duration.
+
+## Customization
+
+The checklist items are stored in a file called `labels.txt`. You can customize the items by editing this file and adding or removing items.
+
+The font used in the GUI can be customized by replacing the `f2.ttf` file in the `fonts` directory with another font file.
+
+## License
+
+This application is licensed under the MIT License. See the `LICENSE` file for details.
--- timer.py
+++ timer.py
@@ -1,39 +1,46 @@
 import dearpygui.dearpygui as dpg
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 
 
 class Timer:
+    """This class represents a timer object that can be used to time
+    events in the GUI."""
+
     def __init__(self, pause_tag, display_tag, time_type_tag):
-        # time attributes is in seconds
         self.time = "0"
         self.started = False
-        self.paused = True  
+        self.paused = True
         self.pause_tag = pause_tag
         self.display_tag = display_tag
         self.time_type = time_type_tag
         self.elapsed_pause = timedelta(0)
-    
-    def advance_time(self):
+
+    def advance_time(self) -> None:
+        """This function is called every frame to update the timer.
+        The dearpygui render loop runs at 60 fps."""
         if self.paused:
             return
-        # The dearpygui render loop runs at 60 fps
         self.time = datetime.now() - self.start_time - self.elapsed_pause
 
-    
-    def reset_timer(self):
+    def reset_timer(self) -> None:
+        """Resets the timer to 0."""
         self.time = "0"
         self.started = False
         self.elapsed_pause = timedelta(0)
-    
-    def resume_timer(self):
+
+    def resume_timer(self) -> None:
+        """Resumes the timer."""
         self.paused = False
         self.elapsed_pause += datetime.now() - self.time_paused
 
-    def pause_timer(self):
+    def pause_timer(self) -> None:
+        """Pauses the timer."""
         self.paused = True
         self.time_paused = datetime.now()
-    
-    def pause_resume_timer(self, sender):
+
+    def pause_resume_timer(self, sender) -> None:
+        """This function is called when the pause/resume button is
+        pressed."""
         if not self.started:
             return
 
@@ -43,42 +50,45 @@ class Timer:
         else:
             self.pause_timer()
 
-    def display_time(self, sender):
-        #hours = self.time.strftime('%H')
+    def display_time(self, sender) -> str:
+        """Returns the time in seconds, minutes, or hours."""
         string_time = str(self.time)
-        format = 'Seconds'
-        if string_time != '0':
-            seconds = float(string_time.split(':')[2])
-            minutes = float(string_time.split(':')[1]) + (seconds/60)
-            hours = float(string_time.split(':')[0]) + (minutes/60)
+        time_format = "Seconds"
+        if string_time != "0":
+            seconds = float(string_time.split(":")[2])
+            minutes = float(string_time.split(":")[1]) + (seconds / 60)
+            hours = float(string_time.split(":")[0]) + (minutes / 60)
             output = seconds
             if minutes >= 1:
                 if hours >= 1:
-                    format = 'Hours'
+                    time_format = "Hours"
                     output = hours
                 else:
-                    format = 'Minutes'
+                    time_format = "Minutes"
                     output = minutes
         else:
             output = 0
-        
-        dpg.set_value(sender, format)
+
+        dpg.set_value(sender, time_format)
         return "{:.2f}".format(output)
 
-    def set_button_label(self, sender):
+    def set_button_label(self, sender) -> None:
+        """Sets the label of the pause/resume button."""
         if self.paused or self.time == 0:
-            label = 'Pause'
+            label = "Pause"
         else:
-            label = 'Resume'
+            label = "Resume"
         dpg.set_item_label(sender, label)
-    
-    def start_timer(self):
+
+    def start_timer(self) -> None:
+        """Starts the timer."""
         if not self.started:
             self.started = True
             self.start_time = datetime.now()
             self.paused = False
 
-    def stop_timer(self):
+    def stop_timer(self) -> None:
+        """Stops and resets the timer."""
         if not self.started:
             return
 
@@ -86,10 +96,3 @@ class Timer:
             self.pause_timer()
         self.reset_timer()
         self.set_button_label(self.pause_tag)
-
-            
-            
-
-
-    
-
